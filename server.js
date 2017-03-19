1227a4a5d2298e15efd249ce4bc3a6cb52687ea0
Dana Ng <ngdana1@gmail.com>
2017-03-19 09:06:20 -0700

    added database and express-validator

--- server.js
+++ server.js
@@ -1,18 +1,62 @@
+// set up local env
+const dotenv = require('dotenv');
+dotenv.load();
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
 const app = express();
 const morgan = require('morgan');
+
+// database & login
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const mongo = require('mongodb');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+mongoose.Promise = global.Promise;
+
 
 const {PORT, DATABASE_URL} = require('./config.js');
 
 // parse json and params in urls
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+// express validator - immediately after bodyParser
+app.use(expressValidator({
+	errorFormater: (param, msg, value) => {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
 
 // provides logging
 app.use(morgan('common'));
 
 app.use('/public', express.static('public'));
+
+// express-session 
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true, // check what this is for?
+  saveUninitialized: true,
+  cookie: { secure: true }
+}));
+
+// passport.js init
+app.use(passport.initalize());
+app.use(passport.session());
 
 
 // routes
