558c5e478c13ea4c2df93dee818a68a293b303d6
Dana Ng <ngdana1@gmail.com>
2017-03-19 17:06:11 -0700

    updated with passport authentication and login and signup routes

--- server.js
+++ server.js
@@ -66,10 +66,101 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+// setup passport local strategy
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+   User.getUserByUsername(username, function(err, user){
+   	if(err) {
+      return done(null, false);
+    }
+
+   	if(!user){
+   		return done(null, false, {message: 'No user found.' });
+      }
+  
+   	User.comparePassword(password, user.password, function(err, isMatch){
+   		 if (err) {
+        return done(err);
+      }
+                
+     		if(isMatch){
+          return done(null, user);
+        } else {
+     			return done(null, false, {message: 'Oops! Wrong password.'});
+        }
+   	});
+    });
+  }
+));
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+passport.deserializeUser(function(id, done) {
+  User.getUserById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+
+
 // routes
 
 app.get('/', (req, res) => {
 	res.render('index');
+});
+
+app.get('/login', (req, res) => {
+	// render login page
+});
+
+app.post('/login', passport.authenticate('local'), {
+	// redirect for failure and success
+});
+
+app.get('/signup', (req, res) => {
+	const name = req.body.name;
+	const email = req.body.email;
+	const username = req.body.username;
+	const password = req.body.password;
+	const password2 = req.body.password2;
+
+	// Validation from expressValidator
+	req.checkBody('name', 'Name is required').notEmpty();
+		req.checkBody('email', 'Email is required').notEmpty();
+		req.checkBody('email', 'Email is not valid').isEmail();
+		req.checkBody('username', 'Username is required').notEmpty();
+		req.checkBody('password', 'Password is required').notEmpty();
+		req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+
+		const errors = req.validationErrors();
+
+	if(errors) {
+	  res.render('register', {
+	    errors: errors
+	  });
+	} else {
+	  const newUser = new User({
+	    name: name,
+	    email: email,
+	    username: username,
+	    password: password
+	  });
+
+	  User.createUser(newUser, (err, user) => {
+	    if(err) {
+	      res.render('register', {error: 'There was an error creating user'} );
+	    }
+	    console.log("User created!");
+	  });
+	 
+	 res.redirect('/login');
+	  // res.render('login', {
+	  //   success_msg: "You are registered and may now login."
+	  // });
+	}
+
 });
 
 app.get('/snippets', (req, res) => {
@@ -82,20 +173,33 @@ app.post('/add-snippet', (req, res) => {
 	res.send('add snippet');
 });
 
-app.put('/update-snippet', (req, res) => {
+app.put('/update-snippet/:snippetid', (req, res) => {
 	// update one snippet
 	res.send('update a snippet');
 });
 
-app.delete('/delete-snippet', (req, res) => {
+app.delete('/delete-snippet/:snippetid', (req, res) => {
 	// delete one snippet
 	res.send('delete a snippet');
 });
 
 
-app.use((req, res) => {
-	res.sendStatus(404);
+// add catch all route for pages that don't have routes
+app.use('*', function(req, res) {
+  return res.status(404).json({message: 'Not Found'});
 });
+
+// functions to ensure authenticated
+
+const ensureauthenticated = (req, res, next) => {
+	if(req.isAuthenticated()) {
+    return next();
+  } else {
+    // req.flash('error_msg', 'You are not logged in');
+    // res.render('home', {layout: "main"});
+    res.redirect('/login');
+  }
+}
 
 // set up server for listening
 
