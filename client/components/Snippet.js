0b0d97504a5d2b5719ab5a53f72680ce30c2a0d1
Dana Ng <ngdana1@gmail.com>
2017-04-02 22:06:40 -0700

    removed console log for snippet, and increased padding for individual snippets - issue with center clicking to getAttribute

--- client/components/Snippet.js
+++ client/components/Snippet.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 export default function Snippets(props) {
-	console.log('props', props);
 	return (
 			<div className="individual-snippets" onClick={props.onClick} data-id={props.id}>
 				<h1>{props.title}</h1>
