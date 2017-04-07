7b8962b242cb0b95110e2319996814b8773b93ca
Dana Ng <ngdana1@gmail.com>
2017-04-06 19:31:28 -0700

    setup for testing

---
+++ test/test.js
@@ -0,0 +1,15 @@
+/** Unit Testing **/
+require('babel-polyfill');
+
+const React = require('react');
+const { shallow } = require('enzyme');
+const { chai } = require('chai');
+
+
+describe('Index page', () => {
+
+	it('should render', () => {
+
+	});
+
+});
