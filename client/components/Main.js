fed6b2f671bf5e06eef409a6f1084d682f5923b8
Dana Ng <ngdana1@gmail.com>
2017-04-10 15:15:34 -0700

    updated styling for main page

--- client/components/Main.js
+++ client/components/Main.js
@@ -11,7 +11,7 @@ class Main extends React.Component {
     }
 
     render() {
-    	
+
         return (
             <div className="main">
             	{!this.props.user && (<nav className="main-nav" id="index-nav">
@@ -22,9 +22,10 @@ class Main extends React.Component {
                      </ul>
                 </nav>)}
                 
-                	<div className="main-slogan">Create code snippets to keep yourself organized</div>
                
-               <div>
+
+                	<div className="main-slogan">Create code snippets to keep yourself organized</div>
+                	 <div>
                       <Link to="/login">
                           <button className="btn">Login</button>
                      </Link>
@@ -34,18 +35,23 @@ class Main extends React.Component {
 
                     </div>
 
+
+               	<img src="https://i.imgur.com/dlRrvfT.png" className="main-image" />
+
+               
+
 	
-				<div className="other-parallax">
 				
-					<div className="row">
+					<div className="row grey-row">
 						<div className="col-6">
-						<img src="https://i.imgur.com/dlRrvfT.png" alt="code image" className="main-image" />
+						<img src="https://github.com/riceball1/simple-code/blob/master/public/icons-for-simple-code/compose.png?raw=true" alt="code image" className="main-icon" />
 						</div>
 						<div className="col-6">
 						<h2>Code Beautiful Snippets</h2>
 						<p>Syntax highlight available in almost any language to keep all your code organized.</p>
 						</div>
-						<div className="col-12"></div>
+						</div>
+						<div className="row grey-row">
 						<div className="col-6">
 						<h2>Search for What You Need</h2>
 						<p>Easily search code snippets and keep organized.</p>
@@ -53,7 +59,8 @@ class Main extends React.Component {
 						<div className="col-6">
 							<img src="https://github.com/riceball1/simple-code/blob/master/public/icons-for-simple-code/search-1.png?raw=true" className="main-icon"/>
 						</div>
-
+					</div> 
+						<div className="row grey-row">
 					<div className="col-6">
 						<img src="https://github.com/riceball1/simple-code/blob/master/public/icons-for-simple-code/cloud-computing-2.png?raw=true" className="main-icon"/>
 						</div>
@@ -61,20 +68,25 @@ class Main extends React.Component {
 							<h2>Keep Code in the Cloud</h2>
 						<p>Keep Code Snippets in one place for easy storage.</p>
 						</div>
-						<div className="col-12">
-							<p className="slogan">Make Snippets. Save. Search.</p>
-						</div>
 					</div>
+					<div className="row">
+					<div className="col-12">
+							<p className="bottom-slogan">Make Snippets. Save. Search.</p>
+						</div>
+						</div>
+					<div className="bottom-main-nav">
+                     <p>Built with {"\u2764"} By <a href="http://www.danafng.com">Dana Ng</a></p>
+                	</div>
 				</div> 
-			</div>
+			
         )
     }
 }
 
 const mapStateToProps = (state) => {
-	return {
-		user: state.userReducer.user
-	}
+    return {
+        user: state.userReducer.user
+    }
 }
 
 export default connect(mapStateToProps)(Main);
