a682a8d87487710df40ac8179a989eaac663dadd
Dana Ng <ngdana1@gmail.com>
2017-04-10 21:46:54 -0700

    updated styling for toggling menu

--- test/test.js
+++ test/test.js
@@ -2,14 +2,24 @@
 require('babel-polyfill');
 
 const React = require('react');
-const { shallow } = require('enzyme');
-const { chai } = require('chai');
+import TestUtils from 'react-addons-test-utils';
+import chai from 'chai';
+
+const should = chai.should();
 
 
-describe('Index page', () => {
+/** components **/
+import Login from '../client/components/Login';
+import Main from '../client/components/Main';
+import Nav from '../client/components/Nav';
+import Sidebar from '../client/components/Sidebar';
+import Signup from '../client/components/Signup';
+import Snippet from '../client/components/Snippet';
+import SnippetForm from '../client/components/SnippetForm';
 
-	it('should render', () => {
 
+describe('Login component', () => {
+	it('Renders the input fields and button', () => {
+		
 	});
-
 });
