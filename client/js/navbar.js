7a5ea411c7cee6406e0579ebe999aeee2aac8cc8
Dana Ng <ngdana1@gmail.com>
2017-04-10 17:17:13 -0700

    fixed scrolling condition

--- client/js/navbar.js
+++ client/js/navbar.js
@@ -1,8 +1,9 @@
 window.addEventListener('scroll', function (e) {
+	console.log(window.innerHeight, document.body.scrollTop)
 	let nav = document.getElementById('index-nav');
-	if (document.documentElement.scrollTop || document.body.scrollTop > window.innerHeight) {
-                nav.classList.add('nav-transparent');
-            } else {
-                nav.classList.remove('nav-transparent');
-            }
+	if (document.documentElement.scrollTop || document.body.scrollTop > 600) {
+        nav.classList.add('nav-transparent');
+    } else {
+        nav.classList.remove('nav-transparent');
+    }
 });
