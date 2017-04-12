4fdca85783b89effcf3b5b54350541cff52b05bb
Dana Ng <ngdana1@gmail.com>
2017-04-11 17:01:46 -0700

    removed console log

--- client/js/navbar.js
+++ client/js/navbar.js
@@ -1,5 +1,4 @@
 window.addEventListener('scroll', function (e) {
-	console.log(window.innerHeight, document.body.scrollTop)
 	let nav = document.getElementById('index-nav');
 	if (document.documentElement.scrollTop || document.body.scrollTop > 600) {
         nav.classList.add('nav-transparent');
